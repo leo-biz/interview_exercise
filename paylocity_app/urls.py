45d091b127b2b04ccb44e07b6cbe3700f7a0ecfe
leo-biz <145124332+leo-biz@users.noreply.github.com>
2023-09-20 09:11:35 -0400

    remove swagger attempt

--- paylocity_app/urls.py
+++ paylocity_app/urls.py
@@ -1,6 +1,5 @@
 from paylocity_app import views
 from django.urls import path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('', views.security_features_list)
