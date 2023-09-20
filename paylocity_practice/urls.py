59c383d0035882dc26bc962d44aef93154547696
leo-biz <145124332+leo-biz@users.noreply.github.com>
2023-09-20 09:11:12 -0400

    remove swagger attempt

--- paylocity_practice/urls.py
+++ paylocity_practice/urls.py
@@ -1,19 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title='Paylocity Exercise API',
-        default_version='1.0.0',
-        description='API Documentation'
-    ), 
-    public=True
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('paylocity_app.urls')),
-    path('api/swagger/schema', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema')
 ]
